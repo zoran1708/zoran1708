b0982812a8597caca1e8557b35aca70451a0c3e1
zoran1708 <zoran.pokupic@gmail.com>
2024-11-13 15:07:49 +0100

    Add files via upload

---
+++ EdunovaWP6/skriptawp6.sql
@@ -0,0 +1,14 @@
+﻿
+--create database edunovawp6;
+
+--drop database edunovawp6;
+
+--use edunovawp6;
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2),
+izvodiseod datetime,
+vaucer bit
+);
---
+++ EdunovaWP6/vjezba1.sql
@@ -0,0 +1,7 @@
+﻿--create database vjezba1;
+use vjezba1;
+create table vjezba1 (
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2)
+);
